ec5358c7f418560500d4b4c39d5552f426fc9aba
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-04-11 15:00:55 +0700

    [#4] - PUSH CODE INTO CLASSES - 59

--- index.ts
+++ index.ts
@@ -18,34 +18,31 @@ enum RawInput {
   UP, DOWN, LEFT, RIGHT
 }
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isUp(): boolean;
-  isDown(): boolean;
+  // isRight(): boolean;
+  // isLeft(): boolean;
+  // isUp(): boolean;
+  // isDown(): boolean;
+  handle(): void;
 }
 class Right implements Input {
-  isRight() { return true; }
-  isLeft() { return false; }
-  isUp() { return false; }
-  isDown() { return false; }
+  handle() {
+    moveHorizontal(1);
+  }
 }
 class Left implements Input {
-  isRight() { return false; }
-  isLeft() { return true; }
-  isUp() { return false; }
-  isDown() { return false; }
+  handle() {
+    moveHorizontal(-1);
+  }
 }
 class Up implements Input {
-  isRight() { return false; }
-  isLeft() { return false; }
-  isUp() { return true; }
-  isDown() { return false; }
+  handle() {
+    moveVertical(-1);
+  }
 }
 class Down implements Input {
-  isRight() { return false; }
-  isLeft() { return false; }
-  isUp() { return false; }
-  isDown() { return true; }
+  handle() {
+    moveVertical(1);
+  }
 }
 
 let playerx = 1;
@@ -124,14 +121,7 @@ function handleInputs() {
 }
 
 function handleInput(input: Input) {
-  if (input.isLeft())
-    moveHorizontal(-1);
-  else if (input.isRight())
-    moveHorizontal(1);
-  else if (input.isUp())
-    moveVertical(-1);
-  else if (input.isDown())
-    moveVertical(1);
+  input.handle();
 }
 
 function updateMap() {
